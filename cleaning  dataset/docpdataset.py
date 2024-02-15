9519d82591f4705df4d347f90677effcb10ee231
mohammedkaled77 <156839254+mohammedkaled77@users.noreply.github.com>
2024-02-15 16:03:22 +0300

    Delete cleaning  dataset directory

--- cleaning  dataset/docpdataset.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-#df= pd.read_csv("C:/Users/Mohammed-Kaled/Desktop/projectsNoorh/noshowappointments.csv")
-df= pd.read_csv("C:/Users/Mohammed-Kaled/Desktop/data/KaggleV2-May-2016.csv ")
-print(df)
