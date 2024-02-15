ddea47bf4c1c381ce76f8b2103a951e97eca7f9e
mohammedkaled77 <156839254+mohammedkaled77@users.noreply.github.com>
2024-02-15 16:34:54 +0300

    Add files via upload

---
+++ cleaning  dataset/docpdataset.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+#df= pd.read_csv("C:/Users/Mohammed-Kaled/Desktop/projectsNoorh/noshowappointments.csv")
+df= pd.read_csv("C:/Users/Mohammed-Kaled/Desktop/data/KaggleV2-May-2016.csv ")
+print(df)
